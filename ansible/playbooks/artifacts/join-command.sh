4136d14643de21eb74478729fec30b8ccef5ae65
zhenechkaaaa <jennyodnost@mail.ru>
2025-10-11 00:01:22 +0700

    changed 02 and 03 playbooks

--- ansible/playbooks/artifacts/join-command.sh
+++ ansible/playbooks/artifacts/join-command.sh
@@ -1,1 +1,4 @@
-kubeadm join 10.10.0.38:6443 --token kss68t.96t9lciw4e1soc9w --discovery-token-ca-cert-hash sha256:a771bf1712541ed981031f82b27b5c80f1341ca705f523a0867c26531a7772d2 
+kubeadm join 10.10.0.38:6443 \
+  --token 0y6lg5.lae4jxgcfbvuxqtn \
+  --discovery-token-ca-cert-hash sha256:a771bf1712541ed981031f82b27b5c80f1341ca705f523a0867c26531a7772d2 \
+  -v=6
